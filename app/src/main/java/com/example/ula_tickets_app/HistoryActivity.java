99e392f1a113ba8ca5822aa7a4dbe1bab3cfbc7b
Vlad Sharafiev <nyam6771@gmail.com>
2025-05-05 20:26:05 +0700

    added history page 1.05

--- app/src/main/java/com/example/ula_tickets_app/HistoryActivity.java
+++ app/src/main/java/com/example/ula_tickets_app/HistoryActivity.java
@@ -18,7 +18,7 @@ public class HistoryActivity extends AppCompatActivity {
     LinearLayout item_list;
 
     @Override
-    protected void onCreate(@NonNull Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_history);
